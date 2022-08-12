52e541461ed6fac1d501be1117d5a9378fa113a2
MadelineRamos <maddieeramos@gmail.com>
2022-08-11 22:30:26 -0400

    set up inital confirm windows and arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,32 @@
 // Assignment code here
 
-// 1. Prompt the user for the password criteria
-//    - password length between 8-128
-//    - lowercase, uppercase, numbers, special characters
-// 2. Validate the input
-// 3. Generate password based on te criteria
-// 4. Display the generated password on the page
-
-
+var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numberArray = ["1","2","3","4","5","6","7","8","9","0"];
+var specialCharactersArray = ["!","#","$","%","&","*","+","/",":",";","<","=",">","?","@","["];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
+  var passwordLength = window.prompt("Please enter the length of your password");
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please choose a number between 8 and 128");
+    passwordLength = window.prompt("Please enter the length of your password");
+  }
+
+  var lowerCase = window.confirm("Would you like to include lowercase letters?");
+  var upperCase = window.confirm("Would you like to include uppercase letters?");
+  var numbers = window.confirm("Would you like to include numbers?");
+  var specialCharacters = window.confirm("Would you like to include special characters?");
+
+  while (!lowerCase && !upperCase && !numbers && !specialCharacters) {
+    window.alert("At least one character type needs to be selected");
+    lowerCase = window.confirm("Would you like to include lowercase letters?");
+    upperCase = window.confirm("Would you like to include uppercase letters?");
+    numbers = window.confirm("Would you like to include numbers?");
+    specialCharacters = window.confirm("Would you like to include special characters?");
+  }
 }
 
 // Write password to the #password input
@@ -20,7 +35,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
